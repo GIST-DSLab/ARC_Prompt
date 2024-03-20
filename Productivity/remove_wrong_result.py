2174839d3a80d49b0ff4765eb246f3818599a556
SejinKimm <ksj614@kaist.ac.kr>
2024-03-20 17:43:14 +0900

    change Python file

--- Productivity/remove_wrong_result.py
+++ Productivity/remove_wrong_result.py
@@ -1,8 +1,8 @@
 import json
-from utils import read_json_file, remove_input_by_order
+from utils import read_data_from_json, remove_input_by_order
 
 file_path = 'HF_Augmented_Data/MoveToBoundary_112_Aug.json'
-data = read_json_file(file_path)
+data = read_data_from_json(file_path)
 
 ###############################################
 order_to_remove = 3 # 삭제하고 싶은 input의 순서
