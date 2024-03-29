4477c9146648cc5fc0b0f440754313b306afba7f
Sejin Kim <sjkim7822@gmail.com>
2024-03-29 17:08:24 +0900

    Update tot_arc_solver.py

--- Compositionality/tot_arc_solver.py
+++ Compositionality/tot_arc_solver.py
@@ -3,10 +3,9 @@ from model.methods.bfs import arc_solve
 from model.tasks.arc import ARCTask
 import json
 import os
-import time
 import openai
 
-# Set variable realted the azure openai. 
+# Set variable related with Azure OpenAI
 openai.deployment_name = os.getenv("AZURE_OPENAI_DEPLOYMENT_NAME") 
 args = argparse.Namespace(backend=openai.deployment_name, temperature=0.7, task='arc', naive_run=False, prompt_sample='standard', method_generate='sample', method_evaluate='value', method_select='greedy', n_generate_sample=3, n_evaluate_sample=1, n_select_sample=2)
 
