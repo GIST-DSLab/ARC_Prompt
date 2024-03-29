9f88134b950af2a990e414c634eb1eb6262ef74e
Sejin Kim <sjkim7822@gmail.com>
2024-03-29 17:12:10 +0900

    Update obj_create.py

--- Compositionality/obj_create.py
+++ Compositionality/obj_create.py
@@ -4,12 +4,14 @@ import json
 import numpy as np
 import copy
 
+
 # type = "tr" or "ev", j_num = 1 ~ 400, pair = "train" or "test" ,p_num = 0 ~ 9, io = 0 ~ 1
 def get_grid ():
     with open("data\\arc_no_object.json", 'r') as f:
         data = json.load(f)
     return data
-    
+
+
 # PnP result is a grid with object indices, so it is necessary to convert it to list of grid coordinate
 def PnP_result_to_grid_coord (PnP_result):
     gg = PnP_result[-1] # the last element of PnP_result is the grid
@@ -29,55 +31,51 @@ def PnP_result_to_grid_coord (PnP_result):
     return obj_coord_list
 
 
+data = get_grid()
+new = []
+# walk through the data and add the objects after every input grid
+for i in range(len(data)):
+    # two exepctions that has error on PnP result
+    if i == 39 or i == 117:
+        pass
         
-
-if __name__ == "__main__":
-    data = get_grid()
-
-    new = []
-    # walk through the data and add the objects after every input grid
-    for i in range(len(data)):
-        # two exepctions that has error on PnP result
-        if i == 39 or i == 117:
-            pass
-            
-        else:
-            new_2 = {}
-            for j in ['train', 'test']:
-                new_3 = []
-                for k in range(len(data[i][j])):
-                    new_4 = {}
-                    for l in ['input', 'output']:
-                        if l == 'input':
-                            obj = PnP.get_object(data[i][j][k][l])
-                            m = 0
-                            new_5 = {}
-                            for m in range(len(PnP_result_to_grid_coord(obj))):
-                                objectname = 'object'+str(m+1)
-                                new_5[objectname] = PnP_result_to_grid_coord(obj)[m]
-
-                            new_4['input'] = data[i][j][k]['input']
-                            new_4['objects'] = new_5    
-                        else: # l == 'output'
-                            new_4['output'] = data[i][j][k]['output']
-
-                    new_3.append(new_4)
-                    
-
-                if j == 'train':
-                    new_2['train'] = new_3
-                else: # j == 'test'
-                    new_2['test'] = new_3
-
-            new.append(new_2)
-
-    # if no file -> create, if file -> overwrite
-    file_path = 'data\\arc.json'
-    if not os.path.exists(file_path):
-        with open(file_path, 'w') as f:
-            json.dump(new, f, indent=4)
     else:
-        print(f"The file {file_path} already exists.")
-        with open(file_path, 'w') as f:
-            json.dump(new, f, indent=4)
-            
+        new_2 = {}
+        for j in ['train', 'test']:
+            new_3 = []
+            for k in range(len(data[i][j])):
+                new_4 = {}
+                for l in ['input', 'output']:
+                    if l == 'input':
+                        obj = PnP.get_object(data[i][j][k][l])
+                        m = 0
+                        new_5 = {}
+                        for m in range(len(PnP_result_to_grid_coord(obj))):
+                            objectname = 'object'+str(m+1)
+                            new_5[objectname] = PnP_result_to_grid_coord(obj)[m]
+
+                        new_4['input'] = data[i][j][k]['input']
+                        new_4['objects'] = new_5    
+                    else: # l == 'output'
+                        new_4['output'] = data[i][j][k]['output']
+
+                new_3.append(new_4)
+                
+
+            if j == 'train':
+                new_2['train'] = new_3
+            else: # j == 'test'
+                new_2['test'] = new_3
+
+        new.append(new_2)
+
+# if no file -> create, if file -> overwrite
+file_path = 'data\\arc.json'
+if not os.path.exists(file_path):
+    with open(file_path, 'w') as f:
+        json.dump(new, f, indent=4)
+else:
+    print(f"The file {file_path} already exists.")
+    with open(file_path, 'w') as f:
+        json.dump(new, f, indent=4)
+        
